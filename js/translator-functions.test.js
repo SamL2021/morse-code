42be46324e84364615c355c650110f168f489553
Samantha Lippiatt <sgd@live.com.au>
2021-12-08 17:13:13 +1100

    Testing changes

--- js/translator-functions.test.js
+++ js/translator-functions.test.js
@@ -3,21 +3,24 @@ import { toMorseCode, toEnglish } from "./translator-functions.js";
 
 // toMorseCode() Testing
 
-describe("Testing toMorseCode()", (x, y) => {
-    it("Should accept lowercase and uppercase letters and return '...'", () => {
+describe("Testing toMorseCode()", () => {
+    it("Should accept lowercase letters and return '...'", () => {
         expect(toMorseCode("s", morse)).toBe("...");
         expect(toMorseCode("sam", morse)).toBe("... .- --");
+    });
+
+    it("Should accept uppercase letters and return '...'", () => {
         expect(toMorseCode("S", morse)).toBe("...");
         expect(toMorseCode("SAM", morse)).toBe("... .- --");
     });
 
-    it("Should accept a '3' or number 2 and return '...--'", (x, y) => {
+    it("Should accept a '3' or number 2 and return '...--'", () => {
         expect(toMorseCode("3", morse)).toBe("...--");
         expect(toMorseCode(3, morse)).toBe("....--");
         expect(toMorseCode("6", morse)).toBe("-....");
     });
 
-    it("Should accept punctuation such as '!' and return '·-·--'", (x, y) => {
+    it("Should accept punctuation such as '!' and return '·-·--'", () => {
         expect(toMorseCode("!", morse)).toBe("·-·--");
         expect(toMorseCode(",", morse)).toBe("--··--");
     });
@@ -56,23 +59,15 @@ describe("Testing toMorseCode()", (x, y) => {
             expect(toEnglish("....-", alphabet)).toBe("4");
         });
 
-        it("Should accept '[]' and throw an error 'Not valid'", () => {
+        it("Any other character apart from letters and numbers should throw an error 'Not a valid entry'", () => {
             expect(toEnglish([], alphabet)).toThrow(
-                "Invalid characters entered. Please only enter string or number."
+                "Invalid data type entered. Please only enter morse code"
             );
-        });
-
-        it("Should accept '{}' and throw an error 'Not valid'", () => {
+            expect(toEnglish("?", alphabet)).toThrow(
+                "Invalid data type entered. Please only enter morse code"
+            );
             expect(toEnglish({}, alphabet)).toThrow(
-                "Invalid characters entered. Please only enter string or number."
-            );
-        });
-
-        it("Should not accept any other character apart from morse code and throw an error 'Not a valid entry'", () => {
-            expect(
-                toEnglish("?", alphabet).toThrow(
-                    "Invalid characters included, cannot translate."
-                )
+                "Invalid data type entered. Please only enter morse code"
             );
         });
 
